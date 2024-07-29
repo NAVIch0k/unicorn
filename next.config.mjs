e18114d55d0c5864e24dfe88d56d82a725a0527a
NAVIchok <ivan.safonov.09@mail.ru>
2024-07-29 12:34:02 +0300

    update start page

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
         return [
             {
                 source: '/',
-                destination: '/login',
+                destination: '/accountslist',
                 permanent: true,
             },
         ]
